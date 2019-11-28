a2e17cea4525992cbd23cf239e41466ff8f2baa2
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2019-11-28 15:50:23 -0500

    plot

--- ejercicios/32/plot.py
+++ ejercicios/32/plot.py
@@ -15,7 +15,7 @@ def grafica(datafile, plotfile, plotlabel):
     n_x = s[1]
     n_t = s[0]
     plt.imshow(data*1E4, extent=[0,1,0.1,0], aspect=10.0)
-    plt.colorbar(label=r"$\psi\times 10^5$")
+    plt.colorbar(label=r"$\psi\times 10^4$")
     plt.xlabel("Posicion")
     plt.ylabel("Tiempo")
     plt.title(plotlabel)
@@ -29,13 +29,13 @@ def grafica(datafile, plotfile, plotlabel):
             plt.plot(x, data[i,:]*1E4, alpha=(i+1)/n_t, color='black', label="t={:.02f}".format(i*delta_t))
     plt.legend(loc=1)
     plt.xlabel("Posicion")
-    plt.ylabel(r"$\psi\times 10^5$")
+    plt.ylabel(r"$\psi\times 10^4$")
             
     plt.subplot(1,3,3)
     t = np.linspace(0,0.1,n_t)
     plt.plot(t, data[:,n_x//4]*1E4, color='black')
     plt.xlabel("Tiempo")
-    plt.ylabel(r"$\psi(L/4)\times 10^5$")
+    plt.ylabel(r"$\psi(L/4)\times 10^4$")
 
 
     plt.savefig(plotfile, bbox_inches='tight')
