1da72ebe0d44bb2cf370f1fdcd75f40530b393b6
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2019-12-14 10:49:34 -0500

    segundo

---
+++ ejercicios/final/manchas.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.fftpack
+
+
+data = np.loadtxt("monthrg.dat")
+fecha = data[:,0] + data[:,1]/12.0
+manchas = data[:,3]
+ii = fecha>1900
+
+# Resto la media para que no domine la frecuencia 0 en la transformada de fourier.
+mean_manchas = np.mean(manchas[ii])
+manchas = manchas[ii] - mean_manchas
+fecha = fecha[ii]
+
+n_points = len(manchas)
+fft_manchas = scipy.fftpack.fft(manchas)
+frecuencias = scipy.fftpack.fftfreq(n_points, 1)
+fft_norm = np.abs(fft_manchas)
+
+# encuentra la frecuencia dominante
+ii = np.argmax(fft_norm)
+periodo_year = (1/frecuencias[ii])/12.0
+
+# Frecuecias mayor a la dominante
+ii_altas = np.abs(frecuencias) > (frecuencias[ii])
+# Frecuecias menores a la dominante
+ii_bajas = np.abs(frecuencias) < (frecuencias[ii])
+
+# Hago cero todos los modos menos los de la frecuencia dominante
+fft_manchas[ii_altas | ii_bajas] = 0.0
+
+# Calculo la transformada inversa
+manchas_dominante = scipy.fftpack.ifft(fft_manchas)
+
+
+ 
+#plt.vlines(frecuencias, np.zeros(n_points), fft_norm, linewidth=4)
+plt.rc('text', usetex=True)
+plt.title(r"El per\'iodo es de {:.2f} a\~nos".format(periodo_year))
+plt.plot(fecha, manchas+ mean_manchas, label='datos')
+plt.plot(fecha, manchas_dominante + mean_manchas, label='frecuencia dominante')
+plt.xlabel('Frecuencia (1/mes)')
+plt.ylabel('Norma FFT')
+plt.grid()
+plt.legend()
+plt.savefig('manchas.png')
