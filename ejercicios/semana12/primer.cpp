5ee1cb102678fcb7203cde596c5cae6a4ed0f0e9
mandaro <jp.mallarino50@uniandes.edu.co>
2020-05-19 13:56:50 -0500

    jpmallarino: week12 matrix multiply

--- ejercicios/semana12/primer.cpp
+++ ejercicios/semana12/primer.cpp
@@ -14,13 +14,18 @@ Encabezado:
 // el namespace de la libreria estandar: std
 using namespace std;
 
-
+int N=10;
+void sum(int a,int b=1); // en el scope: global
+float div(int a,int b=1);
 
 // la funcion main que es la funcion que se ejecuta en el t=0
+// esta funcion SIEMPRE tiene que devolver un numero entero
+// la convencion es que el numero entero que retorna depende de si
+// la funcion se ejecuto correctamente
 int main(){
     // declaro la variable
     // pero NO esta inicializada
-    int i=0,j=0; float x,y=0;
+    int i=0,j=0; float x,y=0; // en el scope: main
     /*
         
         Cada compilador inicializa las variables de una
@@ -57,8 +62,36 @@ int main(){
         cout<<"la variable no es correcta"<<endl;
     }
     
+    // void sum(int a, int b=1) = a+b
+    sum(1,2);
+    sum(5); // 5+1
+    
+    x = div(3,2);
+    cout<<"el valor de la division es: "<<x<<endl;
     // 1. funciones
-    // 2. Arreglos y "apuntadores": strings (C,C++)
-    // 3. Archivos de datos
+    // 2. scoping {}
+    if(i == 1){
+        int k = 3;
+        cout<<k<<endl;
+    } else {
+        int l = 4;
+        cout<<l<<endl;
+    }
+    int k;
+    // 3. arreglos y "apuntadores": strings (C,C++)
+    // 4. archivos de datos
     return 0; // Exit code
+}
+
+
+// <tipo de variable return> "nombre funcion"(parametros){}
+void sum(int a, int b){
+    cout<<"la variable global N es "<<N<<endl;
+    N++; // N=N+1
+    cout<<"la suma es: "<<a+b<<endl;
+}
+
+float div(int a, int b){
+    cout<<"la variable global N es "<<N<<endl;
+    return (float) a/b;
 }
---
+++ ejercicios/semana12/segundo.cpp
@@ -0,0 +1,22 @@
+/*
+
+*/
+
+#include <iostream>
+#include <fstream>
+//#include <string>
+#include <math.h>
+
+using namespace std;
+
+// Multiplicar matrices
+int main(void){
+    // definiciones estaticas
+    int a;
+    float x;
+    double y;
+    // (1,2,3)
+    
+    
+    return 0;
+}
